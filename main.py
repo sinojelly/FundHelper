36bdb384fca391636ac836bdfe3eda687073e345
Jelly Chen <sinojelly@163.com>
2019-12-01 20:07:49 +0800

    add thread for showing progress.

--- main.py
+++ main.py
@@ -8,14 +8,40 @@ from flask import render_template
 
 from fund.FundHelper import update_work_book, str_to_bool
 
+import threading
+import random
+import time
+
 app = Flask(__name__)
+app.debug = True
+exporting_threads = {}
+
+
+class ExportingThread(threading.Thread):
+    def __init__(self):
+        self.progress = 0
+        super().__init__()
+
+    def run(self):
+        # Your exporting stuff goes here ...
+        for _ in range(10):
+            time.sleep(1)
+            self.progress += 10
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'} # fake user
-    return render_template("index.html", title='Home', user=user)
+    # return render_template("index.html", title='Home', user=user)
+    global exporting_threads
+
+    thread_id = random.randint(0, 10000)
+    exporting_threads[thread_id] = ExportingThread()
+    exporting_threads[thread_id].start()
+
+    print('task id: #%s' % thread_id)
+    return render_template("index.html", title='Home', user=user, thread_id=thread_id)
 
 
 @app.route('/update-excel/')
@@ -23,6 +49,13 @@ def index():
 def update_excel(fast_run='True'):
     update_work_book('fund/example_filetest.xlsx', str_to_bool(fast_run))
     return 'Click.' + fast_run
+
+
+@app.route('/progress/<int:thread_id>')
+def progress(thread_id):
+    global exporting_threads
+
+    return str(exporting_threads[thread_id].progress)
 
 
 if __name__ == '__main__':
